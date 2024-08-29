c8d0eb13b1bb595552bdf697ee1f4d12171d403b
swarnava das <swarnavadas67@gmail.com>
2024-08-29 16:03:22 +0000

    My 1st Commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.demo.proddtls.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
